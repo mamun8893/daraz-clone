678af5911e829deb366452b7abdaf0438cb8c438
mamun8893 <0316361936@student.wub.edu.bd>
2022-03-01 23:56:49 +0600

    project setup

---
+++ src/views/pages/common/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <header>
+            <Container>
+                <div className="ic-header-warper">
+                    <div className="logo">
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </Container>
+        </header>
+    );
+};
+
+export default Header;
